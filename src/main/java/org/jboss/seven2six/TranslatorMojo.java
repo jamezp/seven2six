e65880db1369fdeba3e4ce221099dbdf9286bef2
James Perkins <jperkins@redhat.com>
2013-08-30 16:43:26 -0700

    Allow for multiple output directories to be defined.

--- src/main/java/org/jboss/seven2six/TranslatorMojo.java
+++ src/main/java/org/jboss/seven2six/TranslatorMojo.java
@@ -18,14 +18,15 @@ import org.codehaus.plexus.util.DirectoryScanner;
  *
  * @author <a href="mailto:jperkins@redhat.com">James R. Perkins</a>
  */
-@Mojo(name = "transform", defaultPhase = LifecyclePhase.PROCESS_CLASSES)
+@Mojo(name = "transform", defaultPhase = LifecyclePhase.PROCESS_TEST_CLASSES)
 public class TranslatorMojo extends AbstractMojo {
 
     /**
      * The output directory where resources should be processed
      */
-    @Parameter(defaultValue = "${project.build.outputDirectory}", required = true)
-    private File outputDirectory;
+    @Parameter(defaultValue = "${project.build.outputDirectory},${project.build.testOutputDirectory}",
+            property = "seven2six.target.dirs", required = true)
+    private File[] outputDirectories;
 
     /**
      * File patterns to include when processing
@@ -48,24 +49,44 @@ public class TranslatorMojo extends AbstractMojo {
         if (skip) {
             log.info("Skipping seven2six transform");
         } else {
-            final Translator translator = new Translator();
-            final File[] files = getFiles();
+            // Check that the output directories were defined
+            if (outputDirectories != null && outputDirectories.length > 0) {
+                final Translator translator = new Translator();
+                final File[] files = getFiles();
 
-            if (log.isDebugEnabled()) {
-                final String newLine = String.format("%n\t");
-                final StringBuilder sb = new StringBuilder("Transforming Files:");
-                sb.append(newLine);
-                for (File file : files) {
-                    sb.append(file.getAbsolutePath()).append(newLine);
+                if (log.isDebugEnabled()) {
+                    final String newLine = String.format("%n\t");
+                    final StringBuilder sb = new StringBuilder("Transforming Files:");
+                    sb.append(newLine);
+                    for (File file : files) {
+                        sb.append(file.getAbsolutePath()).append(newLine);
+                    }
+                    log.debug(sb);
                 }
-                log.debug(sb);
-            }
 
-            translator.transformRecursive(files);
+                translator.transformRecursive(files);
+            } else {
+                log.warn("No output directories were defined.");
+            }
         }
     }
 
     private File[] getFiles() {
+        final List<File> result = new ArrayList<File>();
+        for (File outputDirectory : outputDirectories) {
+            // If an invalid property was a defined this could result in a null value
+            if (outputDirectory == null) {
+                getLog().warn("A null output directory was found.");
+            } else if (outputDirectory.exists()) {
+                result.addAll(getFiles(outputDirectory));
+            } else {
+                getLog().warn(String.format("Skipping directory '%s' as it does not exist.", outputDirectory.getAbsolutePath()));
+            }
+        }
+        return result.toArray(new File[result.size()]);
+    }
+
+    private List<File> getFiles(final File outputDirectory) {
         final List<File> result = new ArrayList<File>();
         final DirectoryScanner scanner = new DirectoryScanner();
         scanner.setBasedir(outputDirectory);
@@ -79,6 +100,6 @@ public class TranslatorMojo extends AbstractMojo {
                 result.add(targetFile.getAbsoluteFile());
             }
         }
-        return result.toArray(new File[result.size()]);
+        return result;
     }
 }
